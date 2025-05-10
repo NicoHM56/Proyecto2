04ffda92be13760c102a00390e3f461844b87219
NicoHM56 <haa23860@uvg.edu.gt>
2025-05-09 18:03:26 -0600

    Add files via upload

---
+++ proyecto2digital.ino
@@ -0,0 +1,85 @@
+/*
+Autor: Nicole Haase Martínez
+Carnet: 23860
+Fecha de creación: 7/05/2025
+Ultima modificación: 9/05/2025
+
+mejoras del diseño:
+no se esta usando uno de los botones del servo A y B
+los movimientos automatico se puede poner con Ax movimiento1, Ay movimiento2 y así
+*/
+
+#include <Servo.h>
+
+// Nombrar los motores
+Servo myservo_Abajo;
+Servo myservo_lateral_izq;
+Servo myservo_lateral_der;
+Servo myservo_pinza;
+
+// Declaración de Variables
+const int joyAx = A4;
+const int joyAy = A5;
+const int joyAA = 6;
+
+const int joyBx = A2;
+const int joyBy = A3;
+const int joyBB = 7;
+
+const int lauto = 11; // LED modo automático
+const int lmanu = 8;  // LED modo manual
+
+const int modo = 2;   // DIP switch para seleccionar modo
+
+void setup() {
+  // Conectar motores a los pines
+  myservo_Abajo.attach(3);
+  myservo_lateral_izq.attach(5);
+  myservo_lateral_der.attach(9);
+  myservo_pinza.attach(10);
+
+  // Configurar salidas
+  pinMode(lauto, OUTPUT);
+  pinMode(lmanu, OUTPUT);
+
+  // Configurar entrada de modo
+  pinMode(modo, INPUT);
+}
+
+void loop() {
+  
+
+  if (digitalRead(modo) == 0) { // Modo manual
+    digitalWrite(lmanu, HIGH);
+    digitalWrite(lauto, LOW);
+
+    myservo_Abajo.write(movimientoS(joyAx));
+    myservo_lateral_izq.write(movimientoS(joyAy));
+    myservo_lateral_der.write(movimientoS(joyBx));
+    myservo_pinza.write(movimientoS(joyBy));
+
+  } else { // Modo automático
+    digitalWrite(lauto, HIGH);
+    digitalWrite(lmanu, LOW);
+    movimiento_1();
+    movimiento_2();
+    movimiento_3();
+    movimiento_4();
+  }
+  
+}
+
+int movimientoS(int nombre_boton) {
+  int lectura = analogRead(nombre_boton);
+  int angulo = map(lectura, 0, 1023, 0, 180); // Conversión a grados del servo
+  return angulo;
+}
+
+// Funciones automáticas
+void movimiento_1() {
+
+  
+}
+void movimiento_2() {}
+void movimiento_3() {}
+void movimiento_4() {}
