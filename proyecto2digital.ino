b4779f88639ed5d7fab24d4c63e8bccf34b3a6df
NicoHM56 <haa23860@uvg.edu.gt>
2025-05-28 10:36:41 -0600

    Update proyecto2digital.ino

--- proyecto2digital.ino
+++ proyecto2digital.ino
@@ -12,6 +12,8 @@ Se podria hacer más corto el codigo?
 
 #include <Servo.h>
 
+#include <Servo.h>
+
 // Nombrar los motores
 Servo myservo_Abajo;
 Servo myservo_lateral_izq;
@@ -32,7 +34,11 @@ const int lmanu = 8;  // LED modo manual
 
 const int modo = 2;   // DIP switch para seleccionar modo
 
-
+//Angulos de cada servo
+int angulo_Abajo = 90;
+int angulo_Izq = 90;
+int angulo_Der = 90;
+int angulo_Pinza = 90;
 
 // Margen de tolerancia para ignorar ruido
 const int zona_muerta = 80;
@@ -54,15 +60,17 @@ void setup() {
 
 void loop() {
   
+  digitalWrite(lauto, LOW);
+  digitalWrite(lmanu, LOW);
 
   if (digitalRead(modo) == 0) { // Modo manual
     digitalWrite(lmanu, HIGH);
     digitalWrite(lauto, LOW);
 
-    moverGrado(joyAx, 90, myservo_Abajo);
-    moverGrado(joyAy, 90, myservo_lateral_izq);
-    moverGrado(joyBx, 90, myservo_lateral_der);
-    moverGrado(joyBy, 90, myservo_pinza);
+    moverGrado(joyAx, angulo_Abajo, myservo_Abajo);
+    moverGrado(joyAy, angulo_Izq, myservo_lateral_izq);
+    moverGrado(joyBx, angulo_Der, myservo_lateral_der);
+    moverGrado(joyBy, angulo_Pinza, myservo_pinza);
 
     delay(20);  // Pequeña pausa para que el movimiento sea suave
 
@@ -97,12 +105,6 @@ void loop() {
   }
   
 }
-/*
-int movimientoS(int nombre_boton) { //es int ya que retorna un valor entero
-  int lectura = analogRead(nombre_boton);
-  int angulo = map(lectura, 0, 1023, 0, 180); // Conversión a grados del servo
-  return angulo;
-}*/
 
 void moverGrado(int pin, int &angulo, Servo &servo) {
   int lectura = analogRead(pin);  // Lee el valor del joystick
