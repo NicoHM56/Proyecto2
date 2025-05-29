704229736c7163c56f7bfd7b631caed3e085fead
NicoHM56 <haa23860@uvg.edu.gt>
2025-05-28 18:49:06 -0600

    Update proyecto2digital.ino

--- proyecto2digital.ino
+++ proyecto2digital.ino
@@ -10,9 +10,8 @@ los movimientos automatico se puede poner con Ax movimiento1, Ay movimiento2 y a
 Se podria hacer más corto el codigo?
 */
 
-#include <Servo.h>
 
-#include <Servo.h>
+ #include <Servo.h>
 
 // Nombrar los motores
 Servo myservo_Abajo;
@@ -34,7 +33,7 @@ const int lmanu = 8;  // LED modo manual
 
 const int modo = 2;   // DIP switch para seleccionar modo
 
-//Angulos de cada servo
+// Ángulos de cada servo
 int angulo_Abajo = 90;
 int angulo_Izq = 90;
 int angulo_Der = 90;
@@ -50,16 +49,16 @@ void setup() {
   myservo_lateral_der.attach(9);
   myservo_pinza.attach(10);
 
-  // Configurar salidas
   pinMode(lauto, OUTPUT);
   pinMode(lmanu, OUTPUT);
-
-  // Configurar entrada de modo
   pinMode(modo, INPUT);
+
+  // Iniciar comunicación serial
+  Serial.begin(9600);
+  Serial.println("Sistema iniciado");
 }
 
 void loop() {
-  
   digitalWrite(lauto, LOW);
   digitalWrite(lmanu, LOW);
 
@@ -67,86 +66,96 @@ void loop() {
     digitalWrite(lmanu, HIGH);
     digitalWrite(lauto, LOW);
 
+    Serial.println("Modo: MANUAL");
+
     moverGrado(joyAx, angulo_Abajo, myservo_Abajo);
     moverGrado(joyAy, angulo_Izq, myservo_lateral_izq);
     moverGrado(joyBx, angulo_Der, myservo_lateral_der);
     moverGrado(joyBy, angulo_Pinza, myservo_pinza);
 
-    delay(20);  // Pequeña pausa para que el movimiento sea suave
+    // Mostrar ángulos actuales
+    Serial.print("Abajo: "); Serial.print(angulo_Abajo);
+    Serial.print(" | Izq: "); Serial.print(angulo_Izq);
+    Serial.print(" | Der: "); Serial.print(angulo_Der);
+    Serial.print(" | Pinza: "); Serial.println(angulo_Pinza);
+
+    //delay(200); // Delay más largo para ver mejor en serial
 
   } else { // Modo automático
     digitalWrite(lauto, HIGH);
     digitalWrite(lmanu, LOW);
 
-    //llamar a la función
-    int valorJoyAY = analogRead(joyAy);
+    Serial.println("Modo: AUTOMÁTICO");
+
     int valorJoyAX = analogRead(joyAx);
-    int valorJoyBY = analogRead(joyBy);
+    int valorJoyAY = analogRead(joyAy);
     int valorJoyBX = analogRead(joyBx);
+    int valorJoyBY = analogRead(joyBy);
 
-    // cadena de if para llamar cada movimiento
+    // Mostrar valores del joystick
+    Serial.print("joyAX: "); Serial.print(valorJoyAX);
+    Serial.print(" | joyAY: "); Serial.print(valorJoyAY);
+    Serial.print(" | joyBX: "); Serial.print(valorJoyBX);
+    Serial.print(" | joyBY: "); Serial.println(valorJoyBY);
 
-    if (valorJoyAX != 512) { //lo de !=512 es para que funcione cuando el joystic se mueva en cualquier direccion de X
+    if (valorJoyAX != 512) {
+      Serial.println("Ejecutando: movimiento_1");
       movimiento_1();
     }
     else if (valorJoyAY != 512) {
+      Serial.println("Ejecutando: movimiento_2");
       movimiento_2();
     }
     else if (valorJoyBX != 512) {
+      Serial.println("Ejecutando: movimiento_3");
       movimiento_3();
     }
     else if (valorJoyBY != 512) {
+      Serial.println("Ejecutando: movimiento_4");
       movimiento_4();
     }
-    else {
-      //no pasa nada
-    }
-    
+
+    delay(300);
   }
-  
 }
 
 void moverGrado(int pin, int &angulo, Servo &servo) {
-  int lectura = analogRead(pin);  // Lee el valor del joystick
+  int lectura = analogRead(pin);
 
-  // Si el joystick se mueve hacia izquierda/abajo más allá de la zona muerta
-  if (lectura < 512 - zona_muerta) { // eL 512 REPRESENTA 2.5v (0 a 1023)
-    if (angulo > 0) angulo--;        // Resta 1 grado si no está en el mínimo
-    servo.write(angulo);             // Mueve el servo al nuevo ángulo
+  if (lectura < 512 - zona_muerta) {
+    if (angulo > 0) angulo--;
+    servo.write(angulo);
   } 
-  // Si el joystick se mueve hacia la derecha/arriba más allá de la zona muerta
   else if (lectura > 512 + zona_muerta) {
-    if (angulo < 180) angulo++;      // Suma 1 grado si no está en el máximo
-    servo.write(angulo);             // Mueve el servo al nuevo ángulo
+    if (angulo < 180) angulo++;
+    servo.write(angulo);
   }
 }
-
-
-// Funciones automáticas
 void movimiento_1() {
-  myservo_Abajo.write(35);
-  myservo_lateral_izq.write(80);
-  myservo_lateral_der.write(150);
-  myservo_pinza.write(60);
+  myservo_Abajo.write(0);        // totalmente abajo
+  myservo_lateral_izq.write(0);  // completamente a la izquierda
+  myservo_lateral_der.write(180); // completamente a la derecha
+  myservo_pinza.write(30);       // pinza cerrada
 }
 
 void movimiento_2() {
-  myservo_Abajo.write(180);
-  myservo_lateral_izq.write(120);
-  myservo_lateral_der.write(45);
-  myservo_pinza.write(90);
+  myservo_Abajo.write(180);      // totalmente arriba
+  myservo_lateral_izq.write(90); // centro
+  myservo_lateral_der.write(0);  // completamente al otro lado
+  myservo_pinza.write(150);      // pinza abierta
 }
 
 void movimiento_3() {
-  myservo_Abajo.write(90);
-  myservo_lateral_izq.write(35);
-  myservo_lateral_der.write(170);
-  myservo_pinza.write(140);
+  myservo_Abajo.write(90);       // medio
+  myservo_lateral_izq.write(180);
+  myservo_lateral_der.write(90);
+  myservo_pinza.write(0);        // pinza totalmente cerrada
 }
 
 void movimiento_4() {
-  myservo_Abajo.write(150);
+  myservo_Abajo.write(45);       // ángulo intermedio
   myservo_lateral_izq.write(180);
-  myservo_lateral_der.write(60);
-  myservo_pinza.write(35);
+  myservo_lateral_der.write(0);
+  myservo_pinza.write(180);      // totalmente abierta
 }
+
